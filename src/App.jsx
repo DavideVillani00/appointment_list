29405e47ab98bfa1b9e4b41e2cdfc2565cc3bd6d
DavideVillani00 <villani.dado@gmail.com>
2025-03-19 19:22:41 +0100

    first-commit: created and initialized project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <p className="bg-fuchsia-300">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus rem
+        sunt neque nobis doloribus ducimus odio nulla maxime quod qui distinctio
+        quisquam veritatis illo numquam, at inventore, rerum vero perferendis?
+      </p>
+    </>
+  );
+}
+
+export default App;
